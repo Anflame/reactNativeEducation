ec9e755925e16f1ac0412370633bf9d50a42e119
Anflame <anflame@mail.ru>
2022-10-26 11:17:54 +0400

    usual component and handlers

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, Alert, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    console.log('press');
+  }
+
+  const handleButtonPress = () => Alert.alert('title', 'myMessage', [
+    { text: 'Ok', onPress: handlePress},
+    { text: 'Cancel' }
+  ]);
+  const handleButtonPress2 = () => Alert.prompt('title', 'myMessage', text => console.log(text));
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>dsdsds</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={handlePress}>dsdsds{'\n'}sdada</Text>
+      <Button title='Нажми на меня' onPress={handleButtonPress} color='grey' />
+      <Button title='Prompt' onPress={handleButtonPress2} />
+      <TouchableWithoutFeedback onPress={handleButtonPress}>
+        <Image blurRadius={15} source={{
+          width: 200,
+          height: 150,
+          uri: 'https://img3.akspic.ru/previews/9/6/1/9/6/169169/169169-ty_zasluzhivaesh_vsyacheskogo_schastya-schaste-strah-voda-polety_na_vozdushnom_share-500x.jpg'
+        }}/>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,12 +34,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-    textAlign: 'left',
+    alignItems: 'center'
   },
   text: {
-    color: 'white',
+    color: 'red',
     backgroundColor: 'black',
+    textAlign: 'center',
   }
 });

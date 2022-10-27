59cd23cf316762ddcc2442acef721af6b06e6d89
Anflame <anflame@mail.ru>
2022-10-27 12:32:31 +0400

    fixed stlyes

--- pages/styles.ts
+++ pages/styles.ts
@@ -4,9 +4,11 @@ export const styles = StyleSheet.create({
   main: {
     flex: 1,
     padding: 20,
+    paddingTop: 60,
     backgroundColor: 'lightgrey',
   },
   text: {
     fontFamily: 'abrilFatFace',
+    textAlign: 'center'
   }
 });
